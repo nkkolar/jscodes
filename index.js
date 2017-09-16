28dba9398d7c2b533a5588e577a4d5938f2ceb0e
nkkolar <30725624+nkkolar@users.noreply.github.com>
2017-09-16 12:28:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send("Hello world Ð Nikhil here!");
+   res.send("Hello world! Nikhil here!");
 });
 
 app.listen(3000);
